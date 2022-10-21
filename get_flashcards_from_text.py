2497c5a02bb212678e388407963976e1934de8fd
James Mealy <jamesmealy1@gmail.com>
2022-10-21 17:49:56 +0100

    extract difficulty rating to variable

--- get_flashcards_from_text.py
+++ get_flashcards_from_text.py
@@ -9,7 +9,7 @@ translator = Translator()
 # from collections import Counter
 spacy.load('es_core_news_md')
 
-ignored_pos_tags = ['PUNCT', 'SPACE', 'SYM', 'X', 'PROPN', 'NUM']
+FREQUENCY = 4
 
 # Load the data
 nlp = spacy.load('es_core_news_md')
@@ -25,17 +25,18 @@ words = [(
 df = pd.DataFrame(words, columns=['word', 'lemma', 'pos_tag', ])
 df['frequency'] = df['lemma'].apply(
     lambda word: zipf_frequency(word, "es", wordlist='best', minimum=0.0))
-df['included'] = df['frequency'].apply(lambda freq: freq < 4)
+df['included'] = df['frequency'].apply(lambda freq: freq < FREQUENCY)
 sorted = df.sort_values(by=['frequency'], ascending=False)
 filtered = sorted[
-    (sorted['pos_tag'].isin(ignored_pos_tags) == False)
-    # & (sorted['word'] != '-')
+    (sorted['pos_tag'].isin(['PUNCT', 'SPACE', 'SYM', 'X', 'PROPN', 'NUM']) == False)
 ]
 
 target_words = filtered[filtered['included'] == True][['lemma']]
 target_words['translation'] = target_words['lemma'].apply(
     lambda word: translator.translate(word, src='es', dest='en').text)
 csv = target_words.to_csv('./target_words.csv', index=False)
+
+print(filtered.to_string())
 print(target_words)
 
 # words = [
