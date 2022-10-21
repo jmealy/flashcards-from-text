7274a0f0cfad0ce22930b3c6059387c042299437
James Mealy <jamesmealy1@gmail.com>
2022-10-21 18:11:02 +0100

    Improve comments

--- get_flashcards_from_text.py
+++ get_flashcards_from_text.py
@@ -4,14 +4,12 @@ import numpy as np
 import pandas as pd
 from googletrans import Translator
 translator = Translator()
-
-# from spacy import displacy
-# from collections import Counter
 spacy.load('es_core_news_md')
+
 
 FREQUENCY = 4
 
-# Load the data
+# Load the text and process it with spaCy
 nlp = spacy.load('es_core_news_md')
 text = open('./words.txt').read()
 doc = nlp(text)
@@ -23,6 +21,8 @@ words = [(
     token.pos_,
 ) for token in doc]
 df = pd.DataFrame(words, columns=['word', 'lemma', 'pos_tag', ])
+
+# filter and sort by frequency
 df['frequency'] = df['lemma'].apply(
     lambda word: zipf_frequency(word, "es", wordlist='best', minimum=0.0))
 df['included'] = df['frequency'].apply(lambda freq: freq < FREQUENCY)
@@ -31,20 +31,12 @@ filtered = sorted[
     (sorted['pos_tag'].isin(['PUNCT', 'SPACE', 'SYM', 'X', 'PROPN', 'NUM']) == False)
 ]
 
+# Get translations for most difficult (least frequent) words
 target_words = filtered[filtered['included'] == True][['lemma']]
 target_words['translation'] = target_words['lemma'].apply(
     lambda word: translator.translate(word, src='es', dest='en').text)
-csv = target_words.to_csv('./target_words.csv', index=False)
 
+# Save the results in a CSV file for exprt to Anki
+csv = target_words.to_csv('./target_words.csv', index=False)
 print(filtered.to_string())
 print(target_words)
-
-# words = [
-#     ('hacer'),
-#     ('hacer'),
-#     ]
-# df = pd.DataFrame(words, columns=['word'])
-# df['translation'] = df['word'].apply(lambda word:translator.translate(word, src='es', dest='en').text)
-# df['translation'] = df['word'].apply(lambda word: translator.translate(word, src='es', dest='en').text)
-# print(df)
-# translations = [token.text for token in doc]
